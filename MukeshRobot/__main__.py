2d323198e111a48a5c7e85191f7ea0d83540597c
Monster <164752224+Monsterbhai@users.noreply.github.com>
2024-04-25 10:50:52 +0530

    Update __main__.py

--- MukeshRobot/__main__.py
+++ MukeshRobot/__main__.py
@@ -212,7 +212,7 @@ def start(update: Update, context: CallbackContext):
             first_name = update.effective_user.first_name
             
             x=update.effective_message.reply_sticker(
-                "CAACAgUAAx0Cf6QaZwACagFmKeUg6GGwFpFWx8AeBzRfVT7aqAACGAYAAr3-sVTvQXTwBbu_Zh4E)
+                "CAACAgUAAx0Cf6QaZwACagFmKeUg6GGwFpFWx8AeBzRfVT7aqAACGAYAAr3-sVTvQXTwBbu_Zh4E")
             x.delete()
             usr = update.effective_user
             lol = update.effective_message.reply_text(
